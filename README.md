42a52aded0544940769a95d4ef69fa2e54053910
aseverin <severinlx@yahoo.com>
2021-02-19 20:41:22 +0100

    run ec2 in eu

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Learn Terraform Resources
 
 This repo is a companion repo to the [Define Infrastructure with Terraform Resources Learn guide](https://learn.hashicorp.com/tutorials/terraform/resource), containing Terraform configuration files to provision two publicly EC2 instances and an ELB.
+ df -T
+ ssh -i ssh_key.pem ec2-user@ec2-3-125-48-46.eu-central-1.compute.amazonaws.com
--- main.tf
+++ main.tf
@@ -1,21 +1,103 @@
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-central-1"
 }
 
-provider "random" {}
-
-resource "random_pet" "name" {}
-
-resource "aws_instance" "web" {
-  ami           = "ami-a0cfeed8"
-  instance_type = "t2.micro"
-  user_data     = file("init-script.sh")
-
-  tags = {
-    Name = random_pet.name.id
+# Generate new private key
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+}
+# Generate a key-pair with above key
+resource "aws_key_pair" "deployer" {
+  key_name   = "efs-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+# Saving Key Pair for ssh login for Client if needed
+resource "null_resource" "save_key_pair"  {
+  provisioner "local-exec" {
+    command = "echo  ${tls_private_key.my_key.private_key_pem} > mykey.pem"
   }
 }
+
+resource "local_file" "foo" {
+  content     = tls_private_key.my_key.private_key_pem
+  filename = "${path.module}/ssh_key.pem"
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "web-sg"
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "EFS mount target"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t2.micro"
+  user_data     = file("init-script.sh")
+  key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+}
+
+### efs
+resource "aws_efs_file_system" "efs" {
+  creation_token   = "EFS Shared Data"
+  performance_mode = "generalPurpose"
+}
+resource "aws_efs_mount_target" "efs" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = aws_instance.web.subnet_id
+  security_groups = [aws_security_group.web-sg.id]
+}
+
+data "template_file" "script" {
+  template = file("script.tpl")
+  vars = {
+    efs_id = aws_efs_file_system.efs.id
+  }
+}
+
+### output
 
 output "domain-name" {
   value = aws_instance.web.public_dns
